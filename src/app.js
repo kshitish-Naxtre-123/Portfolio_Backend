da9f34f3e0c49e2682d1c116bc73ff5c66b079ea
Kshitish-the-developer-02 <kshitishnayak82@gmail.com>
2024-05-07 17:23:40 +0530

    change origin url

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: "true" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://kshitish-portfolio.netlify.app/",
+    origin: "https://kshitish-portfolio.netlify.app",
     credentials: true,
   })
 );
